59534c88f124cdeedc4b67ee29dca71a74623c35
seom98 <seo_m98@naver.com>
2024-08-26 14:14:56 +0900

    :memo: docs : 프로젝트 세팅

--- src/firebase.js
+++ src/firebase.js
@@ -6,16 +6,16 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "***REMOVED***",
-    authDomain: "***REMOVED***",
-    projectId: "***REMOVED***",
-    storageBucket: "***REMOVED***.appspot.com",
-    messagingSenderId: "***REMOVED***",
-    appId: "1:***REMOVED***:web:abaf0878a7540ef7537f0f",
-    measurementId: "***REMOVED***",
-};
+// const firebaseConfig = {
+//     apiKey: "***REMOVED***",
+//     authDomain: "***REMOVED***",
+//     projectId: "***REMOVED***",
+//     storageBucket: "***REMOVED***.appspot.com",
+//     messagingSenderId: "***REMOVED***",
+//     appId: "1:***REMOVED***:web:abaf0878a7540ef7537f0f",
+//     measurementId: "***REMOVED***",
+// };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
