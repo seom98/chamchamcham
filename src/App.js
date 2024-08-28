bfc1bebfd8987ad8b0e244cc2b8e1aba9cad3c98
seom98 <seo_m98@naver.com>
2024-08-28 15:43:19 +0900

    :memo: docs : 테스트

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React, { useEffect } from "react";
+// firebase.js에서 db를 import
 import { db } from "./firebase";
+import { useEffect, useState } from "react";
+// firestore의 메서드 import
+import { doc, getDoc } from "firebase/firestore";
 
-const App = () => {
+function App() {
+    const [test, setTest] = useState();
+    // async - await로 데이터 fetch 대기
+    async function getTest() {
+        // document에 대한 참조 생성
+        const docRef = doc(db, "test", "t2");
+        // 참조에 대한 Snapshot 쿼리
+        const docSnap = await getDoc(docRef);
+
+        if (docSnap.exists()) {
+            setTest(docSnap.data());
+        } else {
+        }
+    }
+    // 최초 마운트 시에 getTest import
     useEffect(() => {
-        console.log(db);
-    });
-
-    return <div></div>;
-};
+        getTest();
+    }, []);
+    return <div>{test !== undefined && <div>{test.name}</div>}</div>;
+}
 
 export default App;
