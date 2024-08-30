538c6ebfebce735470970cb4d46998ebf6e0a7d5
seom98 <seo_m98@naver.com>
2024-08-30 23:38:38 +0900

    :sparkles: feat : pwa설정

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Router from "./Router";
-
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         <Router />
     </React.StrictMode>
 );
+serviceWorkerRegistration.register();
