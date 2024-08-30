a57b5b4ce6fd8e4146a62b6fd5265a9c6b51b94c
seom98 <seo_m98@naver.com>
2024-08-30 23:58:38 +0900

    :bug: fix : pwa 설정 끝

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Router from "./Router";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
