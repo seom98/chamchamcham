31bac07891f5cf58a60664819181302685d2519b
seom98 <seo_m98@naver.com>
2024-09-01 00:29:31 +0900

    :bug: fix : 다크모드 새로고침시에도 적용

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import "./App.css";
 import { Outlet } from "react-router-dom";
 import Container from "./components/Container";
+import { useEffect } from "react";
 
 export default function App() {
+    useEffect(() => {
+        const bgMode = window.localStorage.getItem("bgMode");
+        if (bgMode === "dark") {
+            document.getElementsByTagName("html")[0].classList.add("dark");
+        }
+    }, []);
     return (
         <Container>
             <Outlet />
