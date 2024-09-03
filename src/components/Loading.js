2958fa62f803b30acb3e7e53f5b301b2f97a195e
seom98 <seo_m98@naver.com>
2024-09-03 22:32:58 +0900

    :sparkles: feat : Loading 화면 개선

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,5 +1,14 @@
-function Loading() {
-    return <div>Loading</div>;
+import Lottie from "lottie-react";
+import loadingLottie from "../assets/lottie/loading2.json";
+import s from "./Loading.module.css";
+
+function Loading({ children }) {
+    return (
+        <div className={s.loadingContainer}>
+            <Lottie animationData={loadingLottie} className={s.loadingGif} />
+            <div className={s.loadingText}>{children}</div>
+        </div>
+    );
 }
 
 export default Loading;
