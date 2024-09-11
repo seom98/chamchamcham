76f2e49a142220600f93d922cd9fdfdcfb16834f
seom98 <seo_m98@naver.com>
2024-09-11 16:30:44 +0900

    :bug: fix : inputmode-> inputMode

--- src/components/pages/welcome/Version.js
+++ src/components/pages/welcome/Version.js
@@ -7,7 +7,7 @@ const Version = React.memo(() => {
     return (
         <PosSL>
             <Text12 $margin={"0 1rem"} $grey>
-                Ver.0.2.6
+                Ver.0.2.7
             </Text12>
         </PosSL>
     );
