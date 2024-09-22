cbd30f62fc42e66b20f5c68dac93ce9d1a27efed
seom98 <seo_m98@naver.com>
2024-09-22 22:31:29 +0900

    :memo: docs : 버전 0.3.6

--- src/components/pages/welcome/Version.js
+++ src/components/pages/welcome/Version.js
@@ -8,7 +8,7 @@ const Version = React.memo(() => {
         <>
             <PosSL>
                 <Text12 $margin={"0 1rem"} $grey>
-                    Ver.0.3.5
+                    Ver.0.3.6
                 </Text12>
             </PosSL>
             <PosSR>
