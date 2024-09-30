c410766d2085bf561e5b27ce4553b2351987bc7c
seom98 <seo_m98@naver.com>
2024-09-30 15:01:34 +0900

    :memo: docs : 버전 0.3.17

--- src/components/pages/setting/AppInformations.js
+++ src/components/pages/setting/AppInformations.js
@@ -30,7 +30,7 @@ const AppInformations = () => {
                 <FlexB $gap={"1rem"}>
                     <Text16>버전</Text16>
                     <Text12 $light $grey $margin={"0 0.5rem"}>
-                        Ver.0.3.16
+                        Ver.0.3.17
                     </Text12>
                 </FlexB>
             </BtnNor>
